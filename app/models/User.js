ce23cf729cbda89aa3a6069e73fb64e513cfbc4f
Olaf <olaf.ruvag@gmail.com>
2022-05-11 15:18:07 -0500

    Borramos comentarios

--- app/models/User.js
+++ app/models/User.js
@@ -1,9 +1,3 @@
-// Requerimiento 1: Se necesita un modelo `User` para guardar la información de un usuario de nuestra app: `id`, `username`, `name`, `bio`, `dateCreated` y `lastUpdated`. 
-// Requerimiento 2: Las propiedades `dateCreated` y `lastUpdated` deberán ser datos de tipo fecha que guarden el momento en que se instancie un nuevo objeto de la clase `User`.
-// Requerimiento 3: Cada objeto tipo user necesita los siguietes gettes: `getUsername`, `getBio`, `getDateCreated`, `getLastUpdated`.
-// Solución: Agrega los getters regresando la propiedad indicada, no olvides usar `this`.
-// Requerimiento 4: Cada objeto necesita los siguientes setters: `setUsername` y `setBio`, para actualizar dichas propiedades.
-// Solución: Agrega los setters usando la palabra `set` y modificando el atributo indicado.
 class User{
     constructor(id, username, name, bio){
         this.id = id
