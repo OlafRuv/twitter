21a9b81914b92eaf27f35f85822cde825fec731c
Olaf <olaf.ruvag@gmail.com>
2022-05-11 14:32:36 -0500

    Creamos la prueba de creacion de usuario con el UserService

---
+++ test/services/UserService.test.js
@@ -0,0 +1,20 @@
+const UserService = require('../../app/services/UserService')
+// Requerimiento 1: Crear un nuevo usuario con lo ya definido en el sprint anterior, usando una nueva clase llamada `UserService`.
+// Criterios de aceptación:
+// 1. Esta clase deberá tener un método `create` que se pueda usar sin instanciar (static method), y que reciba solo los parámetros del `id`, `username` y `name`. 
+// 2. El valor de `bio` deberá ser por default para todos los `user` creados.
+
+// Requerimiento 2: Agregar un nuevo método estático en `UserService` llamado `getInfo` que al recibir un objeto de la clase `User`, me regrese una lista con todos los valores de los atributos de dicho objeto.
+
+// Requerimiento 3: Agregar un nuevo método estático en `UserService` llamado `updateUserUsername`, que reciba un objeto de la clase `User` y un nuevo string, que actualizará el valor de `username`. 
+
+// Requerimiento 4: Agregar un nuevo método estático en `UserService` llamado `getAllUsernames`, que recibirá una lista de objetos de la clase `User`, y regresará la lista de todos los usernames de dichos objetos. 
+
+describe("Test for User Service", () => {
+    test("1) Create a new user using the UserService", () => {
+        const user = UserService.create(1,"OlafRuv","Olaf")
+        expect(user.username).toBe("OlafRuv")
+        expect(user.name).toBe("Olaf")
+        expect(user.bio).not.toBeUndefined()
+    })
+})
